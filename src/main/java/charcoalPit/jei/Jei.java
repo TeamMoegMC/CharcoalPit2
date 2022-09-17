a06c3c73dc2e7c0e3a9e1a794d71ed8b40dbbfb0
dashuaibia <936336596@qq.com>
2022-09-17 22:59:51 +0800

    remove Charcoal jei

--- src/main/java/charcoalPit/jei/Jei.java
+++ src/main/java/charcoalPit/jei/Jei.java
@@ -46,7 +46,6 @@ public class Jei implements IModPlugin {
 		registration.addRecipeCategories(new PotteryRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
 		registration.addRecipeCategories(new OreKilnRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
 		registration.addRecipeCategories(new BloomeryRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
-		registration.addRecipeCategories(new CharcoalPitRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
 		registration.addRecipeCategories(new BarrelRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
 	}
 	
@@ -57,18 +56,15 @@ public class Jei implements IModPlugin {
 	
 	@Override
 	public void registerRecipes(IRecipeRegistration registration) {
-        registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(PotteryKilnRecipe.POTTERY_RECIPE), PotteryRecipeCategory.ID);
-        registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(OreKilnRecipe.ORE_KILN_RECIPE).stream().filter((r) -> !r.output.test(new ItemStack(Items.BARRIER))).collect(Collectors.toList()),
-                OreKilnRecipeCategory.ID);
-        registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(BloomeryRecipe.BLOOMERY_RECIPE), BloomeryRecipeCategory.ID);
-        ArrayList<CharcoalPitRecipeCategory.CharcoalPitRecipe> recipes = new ArrayList<>();
-        recipes.add(new CharcoalPitRecipeCategory.CharcoalPitRecipe(new ItemStack(ModBlockRegistry.LogPile), new ItemStack(Items.CHARCOAL, 9), false));
-        recipes.add(new CharcoalPitRecipeCategory.CharcoalPitRecipe(new ItemStack(ModBlockRegistry.CoalPile), new ItemStack(ModItemRegistry.Coke, 9), true));
-        registration.addRecipes(recipes, CharcoalPitRecipeCategory.ID);
-        registration.addIngredientInfo(Arrays.asList(new ItemStack(Items.CHARCOAL), new ItemStack(ModItemRegistry.Coke), new ItemStack(ModBlockRegistry.LogPile), new ItemStack(ModBlockRegistry.CoalPile)),
-                VanillaTypes.ITEM, I18n.format("charcoal_pit.instruction.build_pit"));
-        registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(BarrelRecipe.BARREL_RECIPE), BarrelRecipeCategory.ID);
-    }
+		registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(PotteryKilnRecipe.POTTERY_RECIPE), PotteryRecipeCategory.ID);
+		registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(OreKilnRecipe.ORE_KILN_RECIPE).stream().filter((r) -> !r.output.test(new ItemStack(Items.BARRIER))).collect(Collectors.toList()),
+				OreKilnRecipeCategory.ID);
+		registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(BloomeryRecipe.BLOOMERY_RECIPE), BloomeryRecipeCategory.ID);
+
+		registration.addIngredientInfo(Arrays.asList(new ItemStack(Items.CHARCOAL), new ItemStack(ModItemRegistry.Coke), new ItemStack(ModBlockRegistry.LogPile), new ItemStack(ModBlockRegistry.CoalPile)),
+				VanillaTypes.ITEM, I18n.format("charcoal_pit.instruction.build_pit"));
+		registration.addRecipes(Minecraft.getInstance().world.getRecipeManager().getRecipesForType(BarrelRecipe.BARREL_RECIPE), BarrelRecipeCategory.ID);
+	}
 	
 	@Override
 	public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
@@ -80,8 +76,6 @@ public class Jei implements IModPlugin {
         registration.addRecipeCatalyst(new ItemStack(ModItemRegistry.Straw), PotteryRecipeCategory.ID);
         registration.addRecipeCatalyst(new ItemStack(ModItemRegistry.ClayPot), OreKilnRecipeCategory.ID);
         registration.addRecipeCatalyst(new ItemStack(ModBlockRegistry.Bellows), BloomeryRecipeCategory.ID);
-        registration.addRecipeCatalyst(new ItemStack(ModBlockRegistry.LogPile), CharcoalPitRecipeCategory.ID);
-        registration.addRecipeCatalyst(new ItemStack(ModBlockRegistry.CoalPile), CharcoalPitRecipeCategory.ID);
         registration.addRecipeCatalyst(new ItemStack(ModBlockRegistry.Barrel), BarrelRecipeCategory.ID);
     }
 	
